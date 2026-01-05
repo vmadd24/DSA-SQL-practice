47ae86111ca67fce8be73bd4fd7689eee37fc09c
Vishwanath Maddula <vishwanathmaddula@gmail.com>
2026-01-05 16:12:25 -0600

    Time: 499 ms (24.01%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,4 +2,5 @@
 
 select w1.id
 from weather w1 inner join weather w2
-    on datediff(W1.recordDate, W2.recordDate) = 1 and w1.temperature > w2.temperature;
+    on datediff(W1.recordDate, W2.recordDate) = 1
+where w1.temperature > w2.temperature;
