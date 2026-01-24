1a9c68fc3802849a2dc19a3b67eda52cf630e3b9
Vishwanath Maddula <vishwanathmaddula@gmail.com>
2026-01-23 21:40:16 -0600

    Time: 647 ms (5.02%) | Memory: 20.2 MB (9.01%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        n = len(nums)
+        permut = []
+        res = []
+        idx = set()
+
+        def compute() -> None:
+
+            if len(idx) == n:
+                if permut in res:
+                    return
+                res.append(permut.copy())
+            
+            for i in range(n):
+
+                if i not in idx:
+                    permut.append(nums[i])
+                    idx.add(i)
+                    compute()
+                    permut.pop()
+                    idx.remove(i)
+        
+        compute()
+        return res
