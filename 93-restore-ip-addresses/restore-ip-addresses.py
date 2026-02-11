addf223ff8da7ad0e15da6ce67d440174aff1799
Vishwanath Maddula <vishwanathmaddula@gmail.com>
2026-02-10 23:17:14 -0600

    Time: 0 ms (100.00%) | Memory: 19.6 MB (6.20%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,45 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        ans = []
+        path = []
+        n = len(s)
+
+        def backtrack(idx: int, numOfSeg: int) -> None:
+
+            nonlocal s, n, path
+
+            if numOfSeg == 4:
+                if idx == n:
+                    ans.append("".join(path.copy())[:-1])
+                return
+
+            
+            for pos in range(1,4):
+
+                if idx+pos>n:
+                    continue
+
+                seg = s[idx:idx+pos]
+                if isValid(seg):
+                    path = path + [seg, "."]
+                    backtrack(idx+pos, numOfSeg+1)
+                    path.pop()
+                    path.pop()
+                
+                if seg == "0":
+                    return
+        
+
+        def isValid(seg: str) -> bool:
+            return 0<=int(seg)<=255
+
+        backtrack(0, 0)
+
+        return ans
+                
+
+
+        
+        
+        
